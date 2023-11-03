9b1d6b0f6442a0a744d62335f87b7413778ca9df
Denis <udevwork@gmail.com>
2023-11-03 10:57:12 +0300

    project

---
+++ esotericApp/esotericApp/esotericAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  esotericAppApp.swift
+//  esotericApp
+//
+//  Created by Denis Kotelnikov on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct esotericAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
